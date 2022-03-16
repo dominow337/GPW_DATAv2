d6ada34fea0346895f0a0cf6b9c37f99642f3f10
dominow337 <43881627+dominow337@users.noreply.github.com>
2022-03-16 12:29:06 +0100

    Add files via upload

---
+++ Informations.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Informations : MonoBehaviour
+{
+    public static bool VisualizationPaused = false;
+
+    public GameObject PauseUI;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ( Input.GetKeyDown(KeyCode.Z))
+        {
+            if (VisualizationPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    void Resume()
+    {
+        PauseUI.SetActive(false);
+        Time.timeScale = 1f;
+        VisualizationPaused = false;
+    }
+
+    void Pause()
+    {
+        PauseUI.SetActive(true);
+        Time.timeScale = 0f;
+        VisualizationPaused = true;
+
+    }
+}
